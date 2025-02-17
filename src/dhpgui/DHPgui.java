677c536c4ab9e5dff242cc53d09a11c39c1d74eb
CarloDestinado <carlodestinado@gmail.com>
2025-02-17 18:38:25 +0800

    mmmmwwwwaaaaa

--- src/dhpgui/DHPgui.java
+++
@@ -1,9 +0,0 @@
-
-package dhpgui;
-
-public class DHPgui {
-
-    public static void main(String[] args) {
-       
-    
-}
---
+++ src/dhpgui/loginSignUp.java
@@ -0,0 +1,14 @@
+
+package dhpgui;
+
+public class loginSignUp {
+
+    public static void main(String[] args) {
+      LoginPanel LoginPanelFrame = new LoginPanel(); 
+              LoginPanelFrame.setVisible(true);
+              LoginPanelFrame.pack();
+              LoginPanelFrame.setLocationRelativeTo(null);
+              
+    
+}
+}
